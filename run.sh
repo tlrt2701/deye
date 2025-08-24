7da1b668e9243092ef881312a4f749b1d050c5aa
tlrt2701 <info@harztec-modellbau.de>
2025-08-24 14:59:16 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,11 @@
 #!/usr/bin/with-contenv bashio
-python3 /sdm630_emulator.py
+
+MQTT_BROKER=$(bashio::config 'mqtt_broker')
+MQTT_TOPIC=$(bashio::config 'mqtt_topic_grid')
+MODBUS_PORT=$(bashio::config 'modbus_port')
+
+export MQTT_BROKER
+export MQTT_TOPIC
+export MODBUS_PORT
+
+exec python3 /sdm630_emulator.py
